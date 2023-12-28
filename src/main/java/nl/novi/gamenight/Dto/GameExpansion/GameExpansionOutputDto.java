c446993a93a21cf1d5eb14d04e26ecf318a81cfe
Barry Kramer <bkramer@snelstart.nl>
2023-12-28 14:12:08 +0100

    Added Input and outputDto
    for Game and GameExpansion

---
+++ src/main/java/nl/novi/gamenight/Dto/GameExpansion/GameExpansionOutputDto.java
@@ -0,0 +1,18 @@
+package nl.novi.gamenight.Dto.GameExpansion;
+
+import nl.novi.gamenight.Model.Game.Category;
+import nl.novi.gamenight.Model.Game.Type;
+
+public class GameExpansionOutputDto {
+    public String name;
+    public String manufacturer;
+    public int minimumPlayers;
+    public int maximumPlayers;
+    public int age;
+    public int minimumDuration;
+    public int averageDuration;
+    public Category category;
+    public Type type;
+    public String averageStarValue;
+    public Long id;
+}
