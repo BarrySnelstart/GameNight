0abc93e91ff92db25a75cb6f4e1911c9a3d3c9df
Barry Kramer <bkramer@snelstart.nl>
2024-01-18 09:02:09 +0100

    Added User model and userRole enum

--- src/main/java/nl/novi/gamenight/Model/User/User.java
+++ src/main/java/nl/novi/gamenight/Model/User/User.java
@@ -1,24 +1,23 @@
-//package nl.novi.gamenight.Model.User;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.Id;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//
-//@Getter
-//@Setter
-//
-//
-//@Entity (name = "users")
-//public class User {
-//    @Id
-//    @GeneratedValue
-//    private long Id;
-//
-//    private String userName;
-//    private String passWord;
-//    private UserRole userRole;
-//
-//}
+package nl.novi.gamenight.Model.User;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
+
+@Entity (name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private long Id;
+
+    private String userName;
+    private String passWord;
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+}
