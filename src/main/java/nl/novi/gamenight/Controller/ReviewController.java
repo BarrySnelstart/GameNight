ee7518dfb9289e1e5ab74af6b84c8823963462c1
Barry Kramer <bkramer@snelstart.nl>
2024-01-31 15:48:32 +0100

    Added DTO Created PostMapping

--- src/main/java/nl/novi/gamenight/Controller/ReviewController.java
+++ src/main/java/nl/novi/gamenight/Controller/ReviewController.java
@@ -1,18 +1,24 @@
 package nl.novi.gamenight.Controller;
 
-import nl.novi.gamenight.Model.Game.Game;
-import nl.novi.gamenight.Model.User.User;
+
+import nl.novi.gamenight.Dto.Game.GameOutputDto;
+import nl.novi.gamenight.Dto.reviewDto.ReviewInputDto;
 import nl.novi.gamenight.Services.ReviewService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("review")
 public class ReviewController {
     public final ReviewService reviewService;
+
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
     }
-
-
+    @PostMapping("/review/{id}")
+    public ResponseEntity<Object> addReview(@Validated @PathVariable ("id")Long id, @RequestBody ReviewInputDto reviewInputDto, BindingResult bindingResult) {
+        return reviewService.addReview(reviewInputDto, bindingResult,id);
+    }
 }
--- src/main/java/nl/novi/gamenight/Services/ReviewService.java
+++ src/main/java/nl/novi/gamenight/Services/ReviewService.java
@@ -1,7 +1,18 @@
 package nl.novi.gamenight.Services;
 
+import nl.novi.gamenight.Dto.Game.GameInputDto;
+import nl.novi.gamenight.Dto.reviewDto.ReviewInputDto;
+import nl.novi.gamenight.Model.Game.Game;
+import nl.novi.gamenight.Model.review.Review;
 import nl.novi.gamenight.Repository.ReviewRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ReviewService {
@@ -10,6 +21,19 @@ public class ReviewService {
         this.reviewRepository = reviewRepository;
     }
 
+    public ResponseEntity<Object> addReview(ReviewInputDto reviewInputDto, BindingResult bindingResult, Long gameId ) {
 
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errors);
+        } else {
+            Review review =new Review();
+            reviewRepository.save(review);
+            return ResponseEntity.created(null).body(review);
+        }
+    }
 
 }
