cf66399e0196eb6115d78e5609e937f212220512
Barry Kramer <bkramer@snelstart.nl>
2024-02-02 12:18:11 +0100

    Added a Post for an expansion.

--- src/main/java/nl/novi/gamenight/Controller/ExpansionController.java
+++ src/main/java/nl/novi/gamenight/Controller/ExpansionController.java
@@ -1,17 +1,25 @@
 package nl.novi.gamenight.Controller;
 
 
+import nl.novi.gamenight.Dto.Game.GameInputDto;
+import nl.novi.gamenight.Services.ExpansionService;
 import nl.novi.gamenight.Services.GameService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("expansion")
 public class ExpansionController {
-    private final GameService gameService;
-
-    public ExpansionController(GameService gameService) {
-        this.gameService = gameService;
+    private final ExpansionService expansionService;
+    public ExpansionController( ExpansionService expansionService) {
+        this.expansionService = expansionService;
     }
 
+    @PostMapping("/{id}")
+    public ResponseEntity<Object> addGameExpansion(@Validated @PathVariable("id")Long gameID, @RequestBody GameInputDto gameInputDto, BindingResult bindingResult) {
+        return expansionService.addGameExpansion(gameInputDto, bindingResult, gameID);
+    }
 
 }
--- src/main/java/nl/novi/gamenight/Services/ExpansionService.java
+++ src/main/java/nl/novi/gamenight/Services/ExpansionService.java
@@ -1,8 +1,67 @@
 package nl.novi.gamenight.Services;
 
+import nl.novi.gamenight.Dto.Game.GameInputDto;
+import nl.novi.gamenight.Dto.Game.GameOutputDto;
+import nl.novi.gamenight.Model.Game.Game;
+import nl.novi.gamenight.Repository.ExpansionRepository;
+import nl.novi.gamenight.Repository.GameRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ExpansionService {
+    GameRepository gameRepository;
+    ExpansionRepository expansionRepository;
+    public ResponseEntity<Object> addGameExpansion(@Validated GameInputDto gameInput, BindingResult bindingResult, Long gameID) {
 
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errors);
+        } else {
+            Game gameExpansion = ToEntity(gameInput);
+            gameRepository.save(gameExpansion);
+            var game = expansionRepository.getReferenceById(gameID);
+            expansionRepository.save(game);
+            return ResponseEntity.created(null).body(game);
+        }
+    }
+
+    public Game ToEntity(GameInputDto gameInput) {
+        var game = new Game();
+        game.setName(gameInput.name);
+        game.setManufacturer(gameInput.manufacturer);
+        game.setMinimumPlayers(gameInput.minimumPlayers);
+        game.setMaximumPlayers(gameInput.maximumPlayers);
+        game.setAge(gameInput.age);
+        game.setMinimumDuration(gameInput.minimumDuration);
+        game.setAverageDuration(gameInput.averageDuration);
+        game.setCategory(gameInput.category);
+        game.setType(gameInput.type);
+        return game;
+    }
+
+    public GameOutputDto toDto(Game game) {
+        GameOutputDto gameOutputDto = new GameOutputDto();
+        gameOutputDto.gameID = game.getGameID();
+        gameOutputDto.name = game.getName();
+        gameOutputDto.manufacturer = game.getManufacturer();
+        gameOutputDto.minimumPlayers = game.getMinimumDuration();
+        gameOutputDto.maximumPlayers = game.getMaximumPlayers();
+        gameOutputDto.age = game.getAge();
+        gameOutputDto.minimumDuration = game.getMinimumDuration();
+        gameOutputDto.averageDuration = game.getAverageDuration();
+        gameOutputDto.category = game.getCategory();
+        gameOutputDto.type = game.getType();
+        gameOutputDto.averageStarValue = game.getAverageStarValue();
+        return gameOutputDto;
+    }
 }
