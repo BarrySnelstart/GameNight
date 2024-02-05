17ac9c48beb611a38ea2d005afaaacecc097e35c
Barry Kramer <bkramer@snelstart.nl>
2024-02-05 20:21:17 +0100

    Added Userflow Crud.
    Added ToDo's

--- src/main/java/nl/novi/gamenight/Controller/UserController.java
+++ src/main/java/nl/novi/gamenight/Controller/UserController.java
@@ -1,6 +1,8 @@
 package nl.novi.gamenight.Controller;
 
 import nl.novi.gamenight.Dto.User.UserInputDto;
+import nl.novi.gamenight.Dto.User.UserOutputDto;
+import nl.novi.gamenight.Model.User;
 import nl.novi.gamenight.Repository.RoleRepository;
 import nl.novi.gamenight.Repository.UserRepository;
 import nl.novi.gamenight.Services.UserService;
@@ -17,17 +19,39 @@ import java.util.List;
 @RestController
 @RequestMapping("users")
 public class UserController {
-     UserService userService;
+    UserService userService;
 
 
     public UserController(UserService userService, RoleRepository roleRepos, PasswordEncoder encoder, UserRepository userRepository) {
         this.userService = userService;
-     }
-
-    @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@Validated @RequestBody UserInputDto userDto, BindingResult validatioResult)
-    {
-        return  (userService.createUser(userDto, validatioResult));
-
     }
+
+    @PostMapping()
+    public ResponseEntity<Object> createUser(@Validated @RequestBody UserInputDto userDto, BindingResult validatioResult) {
+        return (userService.createUser(userDto, validatioResult));
+    }
+
+    /*TODO Admin Only*/
+    @GetMapping
+    public List<UserOutputDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    /*TODO Admin and Owning User Only*/
+    @GetMapping("{id}")
+    public ResponseEntity<Object> getUserByID(@PathVariable("id") Long ID) {
+        return userService.getUserByID(ID);
+    }
+    /*TODO Admin and Owning User Only*/
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteUserByID(@PathVariable("id") Long ID) {
+        return userService.deleteUserByID(ID);
+    }
+    /*TODO Admin and Owning User Only*/
+    @PutMapping("{id}")
+    public ResponseEntity<Object>  updateUserNameByID(@Validated @PathVariable("id") Long ID,@RequestBody UserInputDto updatedUser, BindingResult bindingResult)
+    {
+        return userService.updateUserNameByID(ID, updatedUser, bindingResult);
+    }
+
 }
--- src/main/java/nl/novi/gamenight/Services/UserService.java
+++ src/main/java/nl/novi/gamenight/Services/UserService.java
@@ -1,5 +1,6 @@
 package nl.novi.gamenight.Services;
 
+
 import nl.novi.gamenight.Dto.User.UserInputDto;
 import nl.novi.gamenight.Dto.User.UserOutputDto;
 import nl.novi.gamenight.Model.Role;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.*;
@@ -57,18 +59,70 @@ public class UserService {
         if (rol.isEmpty()) {
             return ResponseEntity.badRequest().body("No UserRoles found");
         }
-        UserOutputDto output = new UserOutputDto();
 
         return ResponseEntity.created(null).body(toDto(newUser));
     }
 
-    public UserOutputDto toDto (User user) {
+    /*TODO Admin Only*/
+    public List<UserOutputDto> getAllUsers() {
+        List<UserOutputDto> allUsersList = new ArrayList<>();
+        for (User user : userRepository.findAll()) {
+            allUsersList.add(toDto(user));
+        }
+        return allUsersList;
+    }
+
+    /*TODO Admin And owning user*/
+    public ResponseEntity<Object> getUserByID(Long gameId) {
+//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        MyUserDetails userDetails = (MyUserDetails) principal;
+        UserOutputDto user = toDto(userRepository.getReferenceById(gameId));
+        return ResponseEntity.created(null).body(user);
+    }
+
+    /*TODO Admin And owning user*/
+    public ResponseEntity deleteUserByID(Long id) {
+        Optional<User> ifExist = userRepository.findById(id);
+        if (ifExist.isPresent()) {
+            userRepository.deleteById(id);
+            return ResponseEntity.ok("User Deleted");
+        } else {
+            return new ResponseEntity<>(new IdNotFoundException("UserID not found in database").getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /*TODO Admin And owning user*/
+    public ResponseEntity<Object> updateUserNameByID(@Validated Long id, UserInputDto updatedUser, BindingResult bindingResult) {
+        Optional<User> ifExist = userRepository.findById(id);
+
+        if (ifExist.isPresent()) {
+            if (bindingResult.hasErrors()) {
+                Map<String, String> errors = new HashMap<>();
+                for (FieldError error : bindingResult.getFieldErrors()) {
+                    errors.put(error.getField(), error.getDefaultMessage());
+                }
+                return ResponseEntity.badRequest().body(errors);
+            } else {
+                var updateUser = userRepository.getReferenceById(id);
+                updateUser.setUsername(updatedUser.username);
+                updateUser.setPassword(encoder.encode(updatedUser.password));
+                userRepository.save(updateUser);
+
+                return ResponseEntity.ok(updatedUser);
+            }
+        } else {
+            return new ResponseEntity<>(new IdNotFoundException("ID not found in database").getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public UserOutputDto toDto(User user) {
         UserOutputDto userOutputDto = new UserOutputDto();
         userOutputDto.userID = user.getUserID();
         userOutputDto.username = user.getUsername();
         return userOutputDto;
     }
-    public User toEntity (UserInputDto userInputDto) {
+
+    public User toEntity(UserInputDto userInputDto) {
         var user = new User();
         user.setUsername(userInputDto.username);
         user.setPassword(encoder.encode(userInputDto.password));
@@ -77,61 +131,8 @@ public class UserService {
 }
 
 
-//        } else {
-//            User user = fromUserInputDtoToEntity(userInput);
-//            userRepository.save(user);
-//            return ResponseEntity.created(null).body(fromEntityToUserOutputDto(user));
-//        }
-//    }
-//
-//    public List<UserOutputDto> getAllUsers() {
-//        List<UserOutputDto> allUsersList = new ArrayList<>();
-//        for (User user : userRepository.findAll()) {
-//            allUsersList.add(fromEntityToUserOutputDto(user));
-//        }
-//        return allUsersList;
-//    }
-//
-//    public UserOutputDto getUserByID(Long id) {
-//        var user = userRepository.getReferenceById(id);
-//        return fromEntityToUserOutputDto(user);
-//    }
-//
-//    public ResponseEntity updateUserNameByID(@Validated Long id, UserInputDto updatedUser, BindingResult bindingResult) {
-//        Optional<User> ifExist = userRepository.findById(id);
-//
-//        if (ifExist.isPresent()) {
-//            if (bindingResult.hasErrors()) {
-//                Map<String, String> errors = new HashMap<>();
-//                for (FieldError error : bindingResult.getFieldErrors()) {
-//                    errors.put(error.getField(), error.getDefaultMessage());
-//                }
-//                return ResponseEntity.badRequest().body(errors);
-//            } else {
-//                var update = userRepository.getReferenceById(id);
-//                User setUpdate = fromUserInputDtoToEntity(updatedUser);
-//                setUpdate.setUsername(updatedUser.username);
-//                setUpdate.setPassword(updatedUser.password);
-//                setUpdate.setUserID(update.getUserID());
-//                userRepository.save(update);
-//
-//                return ResponseEntity.created(null).body(setUpdate);
-//            }
-//        } else {
-//            return new ResponseEntity<>(new IdNotFoundException("ID not found in database").getMessage(), HttpStatus.BAD_REQUEST);
-//        }
-//
-//    }
-//
-//    public ResponseEntity deleteUserByID(Long id) {
-//        Optional<User> ifExist = userRepository.findById(id);
-//        if (ifExist.isPresent()) {
-//            userRepository.deleteById(id);
-//            return ResponseEntity.ok("User Deleted");
-//        } else {
-//            return new ResponseEntity<>(new IdNotFoundException("UserID not found in database").getMessage(), HttpStatus.BAD_REQUEST);
-//        }
-//    }
+
+
 
 
 
