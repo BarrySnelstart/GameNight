69d4c771ab339bfd245cde5d6958bcb797f66523
Barry Kramer <bkramer@snelstart.nl>
2024-02-05 14:55:05 +0100

    Cleanup

--- src/main/java/nl/novi/gamenight/exceptions/IdNotFoundException.java
+++ src/main/java/nl/novi/gamenight/exceptions/IdNotFoundException.java
@@ -2,8 +2,7 @@ package nl.novi.gamenight.exceptions;
 
 public class IdNotFoundException extends RuntimeException {
 
-    public IdNotFoundException() {
-    }
+
 
     public IdNotFoundException(String message) {
         super(message);
