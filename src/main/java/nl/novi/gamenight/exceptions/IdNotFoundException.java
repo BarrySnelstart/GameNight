c6251a685cfe9b7e53b4b5c3e3c88753b9c462cc
Barry Kramer <bkramer@snelstart.nl>
2024-02-07 14:02:10 +0100

    TO in exception handling

--- src/main/java/nl/novi/gamenight/exceptions/IdNotFoundException.java
+++ src/main/java/nl/novi/gamenight/exceptions/IdNotFoundException.java
@@ -2,7 +2,7 @@ package nl.novi.gamenight.exceptions;
 
 public class IdNotFoundException extends RuntimeException {
 
-
+/*TODO default message*/
 
     public IdNotFoundException(String message) {
         super(message);
