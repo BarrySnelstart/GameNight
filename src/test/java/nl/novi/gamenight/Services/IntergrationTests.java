2b07799b107ec1621e922dcf619fce1fa9079fa9
Barry Kramer <bkramer@snelstart.nl>
2024-02-23 13:44:48 +0100

    Added more tests

--- src/test/java/nl/novi/gamenight/Services/IntergrationTests.java
+++ src/test/java/nl/novi/gamenight/Services/IntergrationTests.java
@@ -1,7 +1,11 @@
 package nl.novi.gamenight.Services;
 
+import nl.novi.gamenight.Dto.game.GameOutputDto;
+import nl.novi.gamenight.Model.Category;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
@@ -14,6 +18,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,7 +29,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class IntergrationTests {
     @Autowired
     MockMvc mockMvc;
+    @Mock GameService gameService;
 
+
+    @Test
+    void shouldReturnCorrectGameBasedOnID ()throws Exception{
+        GameOutputDto game = new GameOutputDto();
+        game.gameID = 106L;
+        game.name = "Ganzebordern";
+        game.age = 10;
+        game.averageDuration = 20;
+        game.category = Category.BORD;
+        game.manufacturer = "Jumbo";
+        game.averageStarValue = 4;
+        game.minimumDuration = 10;
+
+
+
+        Mockito.when(gameService.getGameByID(106L)).thenReturn(game);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/game//{gameID}", 106L))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andReturn();
+    }
     @Test
     void GetAllGames() throws Exception {
         this.mockMvc
@@ -49,12 +78,16 @@ public class IntergrationTests {
                      "type":"Computer Game"
                 }
                             """;
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/game/create").contentType(APPLICATION_JSON).content(requestJson)).andDo(MockMvcResultHandlers.print()).andExpect(status().isCreated());
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post("/game/create")
+                .contentType(APPLICATION_JSON)
+                .content(requestJson))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status()
+                .isCreated());
     }
     @Test
-    @Disabled
-        /* Test disabled added Uri return in service*/
-    void getAllGames_Success() throws Exception {
+        void getAllGames_Success() throws Exception {
         // Arrange
 
         // Act
@@ -64,7 +97,8 @@ public class IntergrationTests {
 
         // Assert
         resultActions.andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(12));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()")
+                .value(12));
     }
 
 }
